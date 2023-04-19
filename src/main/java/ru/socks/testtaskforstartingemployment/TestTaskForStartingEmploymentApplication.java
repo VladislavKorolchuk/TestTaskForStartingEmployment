b835e5cb3391c65f9849e45a98c52f222d7fedce
93294 <9329496@mail.ru>
2023-04-19 09:40:05 +0300

    Initial commit

---
+++ src/main/java/ru/socks/testtaskforstartingemployment/TestTaskForStartingEmploymentApplication.java
@@ -0,0 +1,13 @@
+package ru.socks.testtaskforstartingemployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskForStartingEmploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskForStartingEmploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/socks/testtaskforstartingemployment/TestTaskForStartingEmploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.socks.testtaskforstartingemployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskForStartingEmploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
